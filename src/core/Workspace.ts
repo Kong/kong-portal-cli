30612add25bc1d6cbaa28b406ed2941b416ad895
Henri Pietila <henrismail@gmail.com>
2020-04-15 16:44:46 -0700

    fix(Workspace): remove console logs from init

--- src/core/Workspace.ts
+++ src/core/Workspace.ts
@@ -41,29 +41,6 @@ export default class Workspace {
       workspace.config.data.kong_admin_token = process.env.KONG_ADMIN_TOKEN
     }
 
-    console.log('Config:')
-    console.log('')
-    console.log('\t', 'Workspace:', workspace.name)
-
-    if (workspace.config.kongAdminUrl) {
-      console.log(
-        '\t',
-        'Workspace Upstream:',
-        `${workspace.config.kongAdminUrl}/${workspace.name}`,
-        workspace.config.kongAdminToken ? '(authenticated)' : '',
-      )
-    } else if (workspace.config.upstream) {
-      console.log(
-        '\t',
-        'Workspace Upstream:',
-        `${workspace.config.upstream}`,
-        workspace.config.kongAdminToken ? '(authenticated)' : '',
-      )
-    }
-
-    console.log('\t', 'Workspace Directory:', workspace.path)
-    console.log('')
-
     return workspace
   }
 
