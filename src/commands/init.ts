38cfeaba264bf38507329d32d25e56bc631aa93d
Johan Hammar <johan.hammar@gmail.com>
2021-07-29 21:47:10 +0200

    fix(style): fix eslint warnings

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -3,7 +3,7 @@ import { join } from 'upath'
 
 import File from '../core/File'
 
-const cliConf: string =`# Kong Admin URL
+const cliConf = `# Kong Admin URL
 # @required
 kong_admin_url: http://localhost:8001
 
@@ -12,22 +12,18 @@ kong_admin_url: http://localhost:8001
 kong_admin_token: ''`
 
 export default async (args): Promise<void> => {
+  let path = join('workspaces', args.workspace)
 
-  let path = join("workspaces", args.workspace)
+  await fs.ensureDir(path)
 
-  await fs.mkdir(path, { recursive: true }, (err) => {
-    if (err) throw err
-  })
-
-  let file: File = new File(join(path, "cli.conf.yaml"), path)
+  let file: File = new File(join(path, 'cli.conf.yaml'), path)
 
   if (await file.exists()) {
     console.log("cli.conf.yaml already exist in workspace '" + args.workspace + "'")
   } else {
     file.write(cliConf)
 
-    console.log("")
+    console.log('')
     console.log("Workspace '" + args.workspace + "' initialized.")
   }
-  
 }
