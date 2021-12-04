1257075452ed374c628afb1b341ebb0cfd4f75ac
Vincent LE GOFF <vince.legoff@gmail.com>
2021-12-04 16:51:22 +0100

    add types

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -1,8 +1,8 @@
-import { UsageError } from 'clipanion/lib/errors';
-import tap, { Test } from 'tap';
+import { UsageError } from 'clipanion';
+import tap from 'tap';
 import { MissingWorkspaceError } from './helpers'
 
-tap.test('It throws an Usage Error', (t: Test) => {
+tap.test('It throws an Usage Error', t => {
   t.throws(() => { MissingWorkspaceError('foo') }, UsageError)
   t.end();
 })
