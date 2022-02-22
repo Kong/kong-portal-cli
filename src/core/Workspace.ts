5c3b675948acf6d8a70e30b2b77d35a7e4838148
Ludovic Pourrat <52542520+ludovic-pourrat@users.noreply.github.com>
2022-02-22 18:44:51 +0100

    feat(config): KONG_ADMIN_TOKEN_FILE support #99 (#100)

--- src/core/Workspace.ts
+++ src/core/Workspace.ts
@@ -1,5 +1,6 @@
 import * as rs from 'recursive-readdir-async'
 import * as fs from 'fs-extra'
+import { readFileSync } from 'fs'
 import { join } from 'upath'
 
 import WorkspaceConfig from './WorkspaceConfig'
@@ -44,9 +45,19 @@ export default class Workspace {
       workspace.config.data.kong_admin_url = process.env.KONG_ADMIN_URL
     }
 
-    if (process.env.KONG_ADMIN_TOKEN) {
+    if (process.env.KONG_ADMIN_TOKEN_FILE) {
+      if ((await this.exists(process.env.KONG_ADMIN_TOKEN_FILE)) === false) {
+        console.log(`File specified by KONG_ADMIN_TOKEN_FILE is missing at ${process.env.KONG_ADMIN_TOKEN_FILE}`)
+        console.log(``)
+        throw new Error()
+      }
       // eslint-disable-next-line @typescript-eslint/camelcase
-      workspace.config.data.kong_admin_token = process.env.KONG_ADMIN_TOKEN
+      workspace.config.data.kong_admin_token = readFileSync(process.env.KONG_ADMIN_TOKEN_FILE, 'utf-8').trim()
+    } else {
+      if (process.env.KONG_ADMIN_TOKEN) {
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        workspace.config.data.kong_admin_token = process.env.KONG_ADMIN_TOKEN
+      }
     }
 
     if (disableSSLVerification) {
